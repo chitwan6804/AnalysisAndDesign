37fbb3e22906336bb0a38c599b33b0617457207c
Chitwan <128251060+chitwan6804@users.noreply.github.com>
2024-03-12 02:37:47 +0530

    Update multistage.cpp

--- lab6/multistage.cpp
+++ lab6/multistage.cpp
@@ -1,84 +1,74 @@
 #include<iostream>
 #include<cstdlib>
-#include<time.h>
-#include<fstream>
-#include<random>
-#include<algorithm>
 using namespace std;
+int infinity = 10000000;
 
-void WriteDataToFile(int i, double avgTime){
-    ofstream fout("data.txt", ios::app);
-    if(fout.is_open()){
-        fout<<i<<","<<avgTime<<endl;
-        fout.close();
-    }
-    else{
-        cout<<"Unable to open file for writing data. "<<endl;
-    }
-}
-
-int findvertex(int j, int **cost,int fcost[],int n){
-    int min=10000;
-    int r;
-    for(int i=j+1;i<n;i++){
-        if(cost[j][i]!=0){
-            if(cost[j][i]+fcost[i]<=min){
-                r=i;
-                min=cost[j][i]+fcost[i];
+int min_vertex(int key, int cost[5][5], int fcost[5], int n) {
+    int min_cost = 10000;
+    int r = -1; 
+    for(int i = key+1; i<n; i++) {
+        if(cost[key][i] != 0 && cost[key][i] != infinity) {
+            if(cost[key][i] + fcost[i] < min_cost) {
+                min_cost = cost[key][i] + fcost[i];
+                r = i;
             }
-        }
+        } 
     }
     return r;
 }
 
-int findfcost(int **cost,int n, int Path[]){
+void MultiStageGraph(int cost[][5], int n) {
     int fcost[n];
-    fcost[n-1]=0;
-    int distance[n];
-    distance[n-1]=0;
-    for( int j=n-2; j>=0; j--){
-        int r=findvertex(j,cost,fcost,n);
-        fcost[j]=cost[j][r]+fcost[r];
-        distance[j]=r;
+    int R[n];
+    fcost[n-1] = 0; 
+    int r;
+    for(int j = n-2; j >= 0; j--) {
+        r = min_vertex(j, cost, fcost, n);
+        R[j] = r;
+        fcost[j] = cost[j][r] + fcost[r];
     }
-    for(int j=0;j<=n-1;j++){
-        cout<<distance[j]<<endl;
+    cout << "path is : " << endl;
+    cout << "0 ";  // Starting from vertex 0
+    for(int i = 1; i < n-1; i++) {
+        cout << "-> " << R[i] << " ";
     }
-    Path[0]=1;
-    Path[n-2]=n;
-    for(int j=1; j<n-2;j++){
-        Path[j]=distance[Path[j-1]];
-    }
-    for(int j=0;j<n-1;j++){
-        cout<<Path[j]<<endl;
-    }
-    return fcost[0];
+    cout << "-> " << n-1 << endl;
+
+cout<<"min_cost: "<<fcost[0];
 }
-
 int main()
-{   int j;
-    cout<<"Enter number of vertices:";
-    cin>>j;
-    int Vertex[j];
-    for(int i=0;i<=j;i++){
-            Vertex[i]=i+1;
-    }
-
-int** cost = new int*[j];
-    for(int i = 0; i < j; i++) {
-        cost[i] = new int[j];
-    }
-    for(int i=0;i<j;i++){
-        for(int k=0;k<j;k++){
-            cout<<"Cost of Edge from "<<i+1<<" -> "<<k+1<<" :";
-            cin>>cost[i][k];
+{
+    int n;
+    cout<<"Enter total vertices:";
+    int cost[5][5];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<"cost of ["<<i<<"]["<<j<<"] :";
+            cin>>cost[i][j];
         }
     }
-    int Path[j];
-    int mincost=findfcost(cost,j,Path);
-    for(int k=0;k<j-1;k++){
-        cout<<Path[k]<<"-";
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            if(cost[i][j] == -1)
+            {
+                cost[i][j] = infinity;
+            };
+        }
+        cout<<endl;
     }
-    cout<<"minimum cost for this path:"<<mincost;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<cost[i][j];
+        }
+        cout<<endl;
+    }
+    MultiStageGraph(cost,n);
     return 0;
+
 }
