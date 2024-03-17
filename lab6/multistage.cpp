8a6da638d730d66b1c690152d3762a1c50609184
Chitwan <128251060+chitwan6804@users.noreply.github.com>
2024-03-18 04:31:45 +0530

    Update multistage.cpp

--- lab6/multistage.cpp
+++ lab6/multistage.cpp
@@ -1,13 +1,12 @@
 #include<iostream>
 #include<cstdlib>
 using namespace std;
-int infinity = 10000000;
 
-int min_vertex(int key, int cost[5][5], int fcost[5], int n) {
+int min_vertex(int key, int ** cost, int *fcost, int n) {
     int min_cost = 10000;
     int r = -1; 
     for(int i = key+1; i<n; i++) {
-        if(cost[key][i] != 0 && cost[key][i] != infinity) {
+        if(cost[key][i] != 0) {
             if(cost[key][i] + fcost[i] < min_cost) {
                 min_cost = cost[key][i] + fcost[i];
                 r = i;
@@ -17,58 +16,43 @@ int min_vertex(int key, int cost[5][5], int fcost[5], int n) {
     return r;
 }
 
-void MultiStageGraph(int cost[][5], int n) {
+void fcostfind(int ** cost, int n) {
     int fcost[n];
-    int R[n];
+    int d[n];
     fcost[n-1] = 0; 
     int r;
     for(int j = n-2; j >= 0; j--) {
         r = min_vertex(j, cost, fcost, n);
-        R[j] = r;
+        d[r] = j;
         fcost[j] = cost[j][r] + fcost[r];
     }
-    cout << "path is : " << endl;
-    cout << "0 ";  // Starting from vertex 0
+    cout << "Path is as follows:" << endl;
+    cout << "1 ";
     for(int i = 1; i < n-1; i++) {
-        cout << "-> " << R[i] << " ";
+        cout << "->> " << d[i-1] << " ";
     }
-    cout << "-> " << n-1 << endl;
+    cout << "->> " << n << endl;
 
-cout<<"min_cost: "<<fcost[0];
+cout<<"Complete Cost: "<<fcost[0];
 }
+
 int main()
 {
     int n;
     cout<<"Enter total vertices:";
-    int cost[5][5];
-    for(int i=0;i<n;i++)
+    cin>>n;
+        int *cost = new int[n];
+    for (int i = 0; i <n; i++) {
+        cost[i] = new int[n];
+    }
+    for(int i=0;i<n-1;i++)
     {
-        for(int j=0;j<n;j++)
+        for(int j=i+1;j<n;j++)
         {
-            cout<<"cost of ["<<i<<"]["<<j<<"] :";
+            cout<<"cost from vertex "<<i+1<<" to vertex "<<j+1<<":";
             cin>>cost[i][j];
         }
     }
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            if(cost[i][j] == -1)
-            {
-                cost[i][j] = infinity;
-            };
-        }
-        cout<<endl;
-    }
-    for(int i=0;i<n;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            cout<<cost[i][j];
-        }
-        cout<<endl;
-    }
-    MultiStageGraph(cost,n);
+    fcostfind(cost,n);
     return 0;
-
 }
