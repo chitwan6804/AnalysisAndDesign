9bf31abd8db12748867902fcf06a04a47ae12e78
Chitwan <128251060+chitwan6804@users.noreply.github.com>
2024-04-18 14:08:59 +0530

    Add files via upload

---
+++ lab8/LCS.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void LCS(string first,string second){
+    int m=sizeof(first);
+    int n=sizeof(second);
+    for(int i=0;i<=m;i++){
+
+    }
+}
+
+int main(){
+    string first;
+    string second;
+    cout<<"Enter first string: ";
+    cin>>first;
+    cout<<"Enter Second String: ";
+    cin>>second;
+    LCS(first,second);
+}
